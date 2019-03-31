4a7cd1312e2fc14ebf6191f57396837056ee19db
Benson Sanga <bensonsanga@hotmail.com>
2019-03-31 22:35:35 +0200

    Done for the day

--- js/barchart.js
+++ js/barchart.js
@@ -1,7 +1,7 @@
-var w=300;
-var h=100;
+var w=500;
+var h=200;
 var padding= 2;
-var dataset = [5, 20, 16, 23];
+var dataset = [5, 20, 16, 23,5, 20, 16, 23,5, 20, 16, 23];
 var svg = d3.select("body").append("svg")
             .attr("width", w)
             .attr("height", h);
@@ -21,4 +21,18 @@ svg.selectAll("rect")
     })
     .attr("fill", function(d){
         return "rgb(" + (d*10) + ",0,256)";
-    })
+    });
+
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d){return d;})
+.attr("x", function(d, i){
+    return i*(w/dataset.length)+(w/dataset.length-padding)/2;
+})
+.attr("y", function(d){
+    return h - (d*4)+14;
+})
+.attr("text-anchor", "middle")
+.attr("fill","#ffffff")
