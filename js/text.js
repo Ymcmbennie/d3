8d151263716adc085c0d045b2da7a4da59491fa2
Benson Sanga <bensonsanga@hotmail.com>
2019-03-31 16:19:40 +0200

    first Module done

---
+++ js/text.js
@@ -0,0 +1,9 @@
+d3.select("body")
+  .append("svg")
+    .attr("width",250)
+    .attr("height",50)
+  .append("text")
+    .text("Easy too")
+    .attr("x",0)
+    .attr("y",25)
+    .attr("fill","blue");
